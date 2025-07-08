f700508580f5276cd4c962d5ae7b95931618d00e
jordan8080 <117922957+jordan8080@users.noreply.github.com>
2025-07-08 09:47:22 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const postgres = require("postgres");
+const z = require("zod");
+
+const app = express();
+const port = 8000;
+const sql = postgres({ db: "mydb", user: "user", password: "password" });
+
+app.use(express.json());
+
+// Schemas
+const ProductSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  about: z.string(),
+  price: z.number().positive(),
+});
+const CreateProductSchema = ProductSchema.omit({ id: true });
+
+app.post("/products", async (req, res) => {
+  const result = await CreateProductSchema.safeParse(req.body);
+
+  // If Zod parsed successfully the request body
+  if (result.success) {
+    const { name, about, price } = result.data;
+
+    const product = await sql`
+    INSERT INTO products (name, about, price)
+    VALUES (${name}, ${about}, ${price})
+    RETURNING *
+    `;
+
+    res.send(product[0]);
+  } else {
+    res.status(400).send(result);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
